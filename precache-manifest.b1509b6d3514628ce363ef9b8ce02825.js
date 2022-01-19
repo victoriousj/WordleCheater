27e3e710e8a5078c410076de4035468507452bec
vic <victordjohnson@gmail.com>
2022-01-19 17:42:31 -0600

    Updates

---
+++ precache-manifest.b1509b6d3514628ce363ef9b8ce02825.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3def0f938389d316448b61a9116c2f3",
+    "url": "/WordleCheater/index.html"
+  },
+  {
+    "revision": "f7d0b0d3deeab5c9604d",
+    "url": "/WordleCheater/static/css/main.7d51b02c.chunk.css"
+  },
+  {
+    "revision": "7d6dc3e064cc3e796a93",
+    "url": "/WordleCheater/static/js/2.51911834.chunk.js"
+  },
+  {
+    "revision": "f7d0b0d3deeab5c9604d",
+    "url": "/WordleCheater/static/js/main.b49c7b2e.chunk.js"
+  },
+  {
+    "revision": "bb76a4ee41fe3be3e2d3",
+    "url": "/WordleCheater/static/js/runtime-main.00de2595.js"
+  }
+]);
