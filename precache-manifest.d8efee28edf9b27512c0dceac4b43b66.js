e01db48b82ce3dde2634528192e0e6cb3203dd2b
vic <victordjohnson@gmail.com>
2022-01-20 10:26:27 -0600

    Updates

--- precache-manifest.d8efee28edf9b27512c0dceac4b43b66.js
+++ precache-manifest.d8efee28edf9b27512c0dceac4b43b66.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "858711bef7af6c5bb86531baed6f4d68",
+    "revision": "47ad32484beb8a13736003d7c054280c",
     "url": "/WordleCheater/index.html"
   },
   {
-    "revision": "2e403daa968bcad15dbe",
+    "revision": "a6bb750120ed192b9ccd",
     "url": "/WordleCheater/static/css/main.49d47eb1.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/WordleCheater/static/js/2.51911834.chunk.js"
   },
   {
-    "revision": "2e403daa968bcad15dbe",
-    "url": "/WordleCheater/static/js/main.a6cdb84a.chunk.js"
+    "revision": "a6bb750120ed192b9ccd",
+    "url": "/WordleCheater/static/js/main.4cfda7a1.chunk.js"
   },
   {
     "revision": "bb76a4ee41fe3be3e2d3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/WordleCheater/precache-manifest.ec5a8f82affdb4f0df034675d3241de0.js"
+  "/WordleCheater/precache-manifest.d8efee28edf9b27512c0dceac4b43b66.js"
 );
 
 self.addEventListener('message', (event) => {
